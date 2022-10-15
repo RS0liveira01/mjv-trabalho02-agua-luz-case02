c947ca22d59434bb1acf4b6f3fb5ede391d3c408
RS0liveira01 <rdg.oliveira@outlook.com.br>
2022-10-15 18:50:21 -0300

    Classe Endereço Mockada

---
+++ src/main/pessoa/Endereco.java
@@ -0,0 +1,30 @@
+package pessoa;
+
+public class Endereco {
+
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+    private PAIS pais;
+
+    protected Endereco(String[] dados) {
+        // a fazer...
+    }
+
+    private String validarNumero(String numero) {
+        return "a fazer...";
+    }
+
+    private String validarCep(String cep) {
+        return "a fazer...";
+    }
+
+    private String validarPais(String pais) {
+        return "a fazer..."
+    }
+    
+}
