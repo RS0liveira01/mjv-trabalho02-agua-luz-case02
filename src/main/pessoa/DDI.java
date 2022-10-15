bea4b18f7acc03a6148132d34228216687df2671
RS0liveira01 <rdg.oliveira@outlook.com.br>
2022-10-15 18:41:18 -0300

    Enum transferido de telefone para classe própria

---
+++ src/main/pessoa/DDI.java
@@ -0,0 +1,5 @@
+package pessoa;
+
+protected enum DDI{
+    BR("+55"), US("+1"), FR("+33")
+}
--- src/main/pessoa/Telefone.java
+++ src/main/pessoa/Telefone.java
@@ -1,7 +1,6 @@
 package pessoa;
 
-protected enum DDI{
-    BR("+55"), US("+1"), FR("+33")
+
 };
 public class Telefone {
     private String ddd;
