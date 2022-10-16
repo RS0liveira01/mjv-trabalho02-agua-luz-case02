1c3c62c73ff4bb029d0b42f203fff4d2cf225718
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-16 18:26:51 -0300

    mock PessoaJuridica

---
+++ src/main/pessoa/PessoaJuridica.java
@@ -0,0 +1,15 @@
+package pessoa;
+
+public class PessoaJuridica extends Pessoa {
+
+    private String cnpj;
+
+    public PessoaJuridica(String[] dados) {
+        super(dados[1]);
+        // A fazer...
+    }
+
+    private String validarCnpj(String cnpj) {
+        return "A fazer..."
+    }
+}
