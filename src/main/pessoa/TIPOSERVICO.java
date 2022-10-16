0b944c9860ecc74f65a061fe95ec93cceb2e79c8
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-16 17:00:29 -0300

    TIPOSERVICO refatorado como sugerido pelo professor

--- src/main/pessoa/TIPOSERVICO.java
+++ src/main/pessoa/TIPOSERVICO.java
@@ -1,5 +1,38 @@
 package pessoa;
 
 public enum TIPOSERVICO {
-    AGUA, LUZ
+    AGUA("A", 115.0),
+    LUZ("L", 127.0);
+
+    private String sigla;
+    private Double valor;
+
+    private TIPOSERVICO (String sigla, Double valor){
+        this.sigla = sigla;
+        this.valor = valor;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public static TIPOSERVICO valueOfSigla(String sigla){
+        for(TIPOSERVICO ts : values()){
+            if(ts.getSigla().equals(sigla))
+                return ts;
+        }
+        throw new IllegalArgumentException("Não foi possível localizar um tipo serviço com a sigla " + sigla);
+    }
+
+    public static void main(String[] args) {
+        TIPOSERVICO byLabel = TIPOSERVICO.valueOf("AGUA");//padrao do java todo enum possue este método, mas tem que ser EXATO
+        System.out.println(byLabel.getValor());
+
+        TIPOSERVICO bySigla = TIPOSERVICO.valueOfSigla("L"); // customizando a busca por um atributo
+        System.out.println(bySigla.getValor());
+    }
 }
