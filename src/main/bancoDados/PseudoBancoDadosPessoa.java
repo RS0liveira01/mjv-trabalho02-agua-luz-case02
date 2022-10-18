f865f0bb176b8b757908b54bae1d0d49deea2f21
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-18 09:27:57 -0300

    classe PseudoBancoDadosPessoa

---
+++ src/main/bancoDados/PseudoBancoDadosPessoa.java
@@ -0,0 +1,11 @@
+package bancoDados;
+
+import pessoa.Pessoa;
+
+public class PseudoBancoDadosPessoa {
+    ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+
+    public ArrayList<Pessoa> getPessoas() {
+        return pessoas;
+    }
+}
