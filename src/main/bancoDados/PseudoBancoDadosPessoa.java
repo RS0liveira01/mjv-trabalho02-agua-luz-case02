d024a80493f09ef1f3feeb0bbc76869663db33fe
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-18 09:37:22 -0300

    método getPessoaPorNome

--- src/main/bancoDados/PseudoBancoDadosPessoa.java
+++ src/main/bancoDados/PseudoBancoDadosPessoa.java
@@ -2,8 +2,26 @@ package bancoDados;
 
 import pessoa.Pessoa;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class PseudoBancoDadosPessoa {
     ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+
+    public void registrarPessoa(Pessoa pessoa) {
+        this.pessoas.add(pessoa);
+    }
+
+    public Pessoa getPessoaPorNome(String nome) {
+        int indicePesquisa = -1;
+
+        // Perguntar porque equals e não ==
+        for(int i = 0; (i > this.pessoas.size()) || (indicePesquisa == -1); i++)
+            if(Objects.equals(this.pessoas.get(i).getNome(), nome))
+                indicePesquisa = i;
+
+        return this.pessoas.get(indicePesquisa);
+    }
 
     public ArrayList<Pessoa> getPessoas() {
         return pessoas;
