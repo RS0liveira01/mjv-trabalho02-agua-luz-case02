48d53f4297ecba22ce1710eee60bdf3ddc187f67
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-18 10:02:37 -0300

    refatorando getPessoaPorNome

--- src/main/bancoDados/PseudoBancoDadosPessoa.java
+++ src/main/bancoDados/PseudoBancoDadosPessoa.java
@@ -1,5 +1,6 @@
 package bancoDados;
 
+import errors.PessoaNorFoundException;
 import pessoa.Pessoa;
 
 import java.util.ArrayList;
@@ -13,12 +14,13 @@ public class PseudoBancoDadosPessoa {
     }
 
     public Pessoa getPessoaPorNome(String nome) {
-        int indicePesquisa = -1;
-
         // Perguntar porque equals e não ==
-        for(int i = 0; (i > this.pessoas.size()) || (indicePesquisa == -1); i++)
+        for(int i = 0; i < this.pessoas.size(); i++)
             if(Objects.equals(this.pessoas.get(i).getNome(), nome))
-                indicePesquisa = i;
+                return this.pessoas.get(i);
+
+        throw new PessoaNorFoundException();
+    }
 
         return this.pessoas.get(indicePesquisa);
     }
