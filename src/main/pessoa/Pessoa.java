8825e6c72421dba253d435bade49ebb3072ed664
lucassilvasouz <108194718+lucassilvasouz@users.noreply.github.com>
2022-10-18 20:33:17 -0400

    Inseri mudanças na classe pessoas

--- src/main/pessoa/Pessoa.java
+++ src/main/pessoa/Pessoa.java
@@ -1,37 +1,71 @@
 package pessoa;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import pessoa.Telefone;
 
 public abstract class Pessoa {
-    private String nome;
-    private ArrayList<Telefone> telefones;
-    private ArrayList<Contrato> contratos;
+	private String nome;
+	private ArrayList<Telefone> telefones;
+	private ArrayList<Contrato> contratos;
+	private Endereco endereco;
+	private Contrato contrato;
 
-    protected Pessoa(String nome) {
-        this.nome = validarNome(nome);
-    }
+	protected Pessoa(String nome) {
+		this.nome = validarNome(nome);
+	}
 
-    private String validarNome(String nome) {
-        return "a fazer...";
-    }
+	private void validarNome(String nome) {
+		if(nome.isBlank()|| nome.isEmpty()){
+			throw new InvalidNomeException("Nome vazio ou em branco");
+		}
+		if (nome.length()> 36) {
+			throw new InvalidNomeException("Valor supera tamanho permitido");
+		}
+		Pattern padrao = Pattern.compile("[a-zA-Z]*");
+		Matcher regex = padrao.matcher(nome);
+		if(!regex.matches()){
+			throw new InvalidNomeException("O nome contém números ou caracteres especiais");
+		}
+	}
 
-        public void registrarContrato(String[] dados){
-        // A fazer...
-    }
+	protected void registrarContrato(String[] dados){
+		this.registrarTelefone(dados[0]);
+		
+		endereco.setLogradouro(dados[1]);
+		endereco.setNumero(dados[2]);	
+		endereco.setComplemento(dados[3]);
+		endereco.setBairro(dados[4]);
+		endereco.setCidade(dados[5]);
+		endereco.setUf(dados[6]);
+		endereco.setCep(dados[7]);
+		endereco.setPais(PAIS.valueOf(dados[8]));
+		contrato.setProtocolo(dados[9]);
+		contrato.setDataHora(LocalDateTime.now());
+		contrato.setTipoServico(TIPOSERVICO.valueOf(dados[11]));
+		contrato.setTipoNotificacao(TIPONOTIFICACAO.valueOf(dados[13]));
+				
+	}
 
-    public void registrarTelefone(String telefone) {
-        // A fazer...
-    }
+	protected void registrarTelefone(String telefone) {
+		
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public ArrayList<Telefone> getTelefones() {
-        return telefones;
-    }
+	public ArrayList<Telefone> getTelefones() {
+		return telefones;
+	}
 
-    public ArrayList<Contrato> getContratos() {
-        return contratos;
-    }
+	public ArrayList<Contrato> getContratos() {
+		return contratos;
+	}
 }
