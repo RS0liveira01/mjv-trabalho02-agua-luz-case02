8c331db14e78ea7dfdce8db854c2807d86aadafe
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-19 09:08:11 -0300

    classe LeitorArquivoPessoa

---
+++ src/main/gereciarArquivos/LeitorArquivoPessoa.java
@@ -0,0 +1,10 @@
+package gereciarArquivos;
+
+import java.util.ArrayList;
+
+public class LeitorArquivoPessoa {
+    public static ArrayList<String[]> lerArquivo(String pathArquivo) {
+        ArrayList<String[]> infoContratos = new ArrayList<String[]>();
+        return infoContratos;
+    }
+}
