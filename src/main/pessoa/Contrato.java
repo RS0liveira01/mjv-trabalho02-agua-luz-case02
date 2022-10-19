0feb4e028333ad790d33b052005e31e965a28d1e
RS0liveira01 <rdg.oliveira@outlook.com.br>
2022-10-18 23:55:38 -0300

    Trabalhando nos metodos de Contrato

--- src/main/pessoa/Contrato.java
+++ src/main/pessoa/Contrato.java
@@ -1,6 +1,16 @@
 package pessoa;
 
+import errors.InvalidDateFormatException;
+import errors.InvalidPhoneNumberException;
+import errors.InvalidProtocolException;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class Contrato {
 
@@ -16,14 +26,58 @@ public class Contrato {
     }
 
     private String validarProtocolo(String protocolo) {
-        return "a fazer...";
+
+        //validar se protocolo possui 7 digitos
+        if(protocolo.length() != 7) {
+            throw new InvalidProtocolException();
+        }
+
+        //validar se protocolo possui somente numeros
+        if(!ENumero.verificaString(protocolo)) {
+            throw new InvalidProtocolException();
+        }
+
+        return protocolo;
     }
 
     private LocalDateTime validarDataHora(String[] dataHora) {
+        //validar se hora e minutos são válidos
+
+        //validar se o formato de data é valido
+        String strDate = String.valueOf(dataHora);
+        String dateFormat = "dd/MM/uuuu";
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+                .ofPattern(dateFormat)
+                .withResolverStyle(ResolverStyle.STRICT);
+
+        try {
+            LocalDate date = LocalDate.parse(strDate, dateTimeFormatter);
+
+        } catch (DateTimeParseException e) {
+            throw new InvalidDateFormatException();
+
+        }
+
         return LocalDateTime.now();
     }
 
     private FUSOHORARIO determinarFusoHorario(String pais) {
+
+        private PAIS validarPais(String pais) {
+
+            try {
+                PAIS paisValido = PAIS.valueOf(pais);
+                return paisValido;
+            } catch (IllegalArgumentException ex) {
+                throw new InvalidPaisException();
+            }
+
+        }
+
+
+
+
         return FUSOHORARIO.BR;
     }
 
--- src/main/principal/Principal.java
+++ src/main/principal/Principal.java
@@ -2,8 +2,34 @@ package principal;
 
 import pessoa.Telefone;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String strDate = "07/23/1995";
+        String dateFormat = "dd/MM/uuuu";
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+                .ofPattern(dateFormat)
+                .withResolverStyle(ResolverStyle.STRICT);
+
+        try {
+            LocalDate date = LocalDate.parse(strDate, dateTimeFormatter);
+            System.out.println("deu certo");;
+        } catch (DateTimeParseException e) {
+            System.out.println("data invalida");;
+
+        }
+
     }
+
+
+
 }
+
