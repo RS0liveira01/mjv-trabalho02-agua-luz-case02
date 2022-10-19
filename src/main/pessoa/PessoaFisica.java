e9454d570c2dcf03c883262df4a84da112ed340a
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-19 11:48:21 -0300

    revisão PessoaFisica

--- src/main/pessoa/PessoaFisica.java
+++ src/main/pessoa/PessoaFisica.java
@@ -12,53 +12,58 @@ public class PessoaFisica extends Pessoa {
 
     public PessoaFisica(String[] dados) {
         super(dados[2]);
-        // A fazer...
+
+        this.cpf = this.validarCpf(dados[0]);
+        this.rg = this.validarRg(dados[1]);
     }
 
     private String validarCpf(String cpf) {
         if (cpf.isEmpty()) {
             throw new InvalidCpfException();
         }
+
         if (cpf.length() > 14) {
             throw new InvalidCpfException();
         }
+
         if (cpf.length() < 14){
             throw new InvalidCpfException();
         }
+
         Pattern padrao = Pattern.compile("[0-9.--]*");
         Matcher regex = padrao.matcher(cpf);
         if (!regex.matches()) {
             throw new InvalidCpfException();
         }
+
         return cpf;
     }
-
 
     private String validarRg(String rg) {
         if (rg.isEmpty()) {
             throw new InvalidRgException();
         }
+
         if (rg.length() >8) {
             throw new InvalidRgException();
         }
+
         if (rg.length() <8) {
             throw new InvalidRgException();
         }
+
         Pattern padrao = Pattern.compile("[0-9.--]*");
         Matcher regex = padrao.matcher(cpf);
         if (!regex.matches()) {
             throw new InvalidRgException();
         }
-        return rg;
-        }
 
-        public String getCpf(){
-        return cpf;
-        }
-
-        public String getRg(){
         return rg;
-        }
+    }
+
+    public String getCpf() { return cpf; }
+
+    public String getRg(){ return rg; }
 }
 
 
