ade3ada19f3005e65df4f8e69fec9db70b05b726
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-19 11:05:25 -0300

    revisão TIPONOTIFACACAO

--- src/main/pessoa/TIPONOTIFICACAO.java
+++ src/main/pessoa/TIPONOTIFICACAO.java
@@ -1,5 +1,12 @@
 package pessoa;
 
 public enum TIPONOTIFICACAO {
-    SMS, WHATS
+    SMS("SMS"),
+    WHATS("WhatsApp");
+
+    public final String valor;
+
+    private TIPONOTIFICACAO(String valor) {
+        this.valor = valor;
+    }
 }
