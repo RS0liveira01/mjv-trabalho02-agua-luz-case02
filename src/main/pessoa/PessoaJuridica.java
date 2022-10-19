6f14a0884dedfc5930e0420f9fb50167fe734aa7
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-19 12:42:53 -0300

    refatoração de validacaoCnpj para retornar o CNPJ formatado

--- src/main/pessoa/PessoaJuridica.java
+++ src/main/pessoa/PessoaJuridica.java
@@ -21,21 +21,35 @@ public class PessoaJuridica extends Pessoa {
             throw new InvalidCnpjException();
         }
 
-        if (cnpj.length() < 18) {
+        if (cnpj.length() != 16) {
             throw new InvalidRgException();
         }
 
-        if (cnpj.length() > 18) {
-            throw new InvalidRgException();
-        }
-
-        Pattern padrao = Pattern.compile("[0-9.-/-]*");
+        Pattern padrao = Pattern.compile("[0-9]*");
         Matcher regex = padrao.matcher(cnpj);
         if (!regex.matches()) {
             throw new InvalidRgException();
         }
 
-        return cnpj;
+        String cnpjPrimeiraSerie = cnpj.substring(0, 2);
+        String cnpjSegundaSerie = cnpj.substring(2, 5);
+        String cnpjTerceiraSerie = cnpj.substring(5, 8);
+        String cnpjPosBarra = cnpj.substring(8, 12);
+        String cnpjDigito = cnpj.substring(13);
+
+        String cnpjFormatado = (
+                cnpjPrimeiraSerie
+                + "."
+                + cnpjSegundaSerie
+                + "."
+                + cnpjTerceiraSerie
+                + "/"
+                + cnpjPosBarra
+                + "-"
+                + cnpjDigito
+        );
+
+        return cnpjFormatado;
     }
 
 
