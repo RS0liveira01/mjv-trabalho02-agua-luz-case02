020497adc8b01f605f1b61abdc9fa311aa5b60cc
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-19 09:08:31 -0300

    método lerArquivo

--- src/main/gereciarArquivos/LeitorArquivoPessoa.java
+++ src/main/gereciarArquivos/LeitorArquivoPessoa.java
@@ -1,10 +1,26 @@
 package gereciarArquivos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class LeitorArquivoPessoa {
-    public static ArrayList<String[]> lerArquivo(String pathArquivo) {
+    public static ArrayList<String[]> lerArquivo(String pathArquivo) throws IOException {
         ArrayList<String[]> infoContratos = new ArrayList<String[]>();
+        Path pathToFile = Paths.get(pathArquivo);
+
+        BufferedReader bufferLeitor = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII);
+        String line = bufferLeitor.readLine();
+
+        while(line != null) {
+            infoContratos.add(line.split(";"));
+            line = bufferLeitor.readLine();
+        }
+
         return infoContratos;
     }
 }
